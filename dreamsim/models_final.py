30957dd22c14d88690039494abacc01effd3714e
Ge Yang <yangge1987@gmail.com>
2023-10-30 22:32:34 -0400

    add clean commit, cherry picked

--- dreamsim/models_final.py
+++ dreamsim/models_final.py
@@ -4,11 +4,11 @@ class MLP(nn.Sequential):
     def __init__():
         super().__init__(
             nn.Linear(10, 200),
-            nn.Tahn(),
+            nn.Sin(),
             nn.Linear(200, 200),
-            nn.Tahn(),
+            nn.Sin(),
             nn.Linear(200, 200),
-            nn.Tahn(),
+            nn.Sin(),
             nn.Linear(200, 10),
         )
 
